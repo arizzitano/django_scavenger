9ed6dd789f7eda3d6edde1ec42640ebb2a2fac97
Ariana Rizzitano <arizzitano@gmail.com>
2012-08-14 13:34:20 -0400

    started adding admin view, swapped domain

--- django_scavenger/views.py
+++ django_scavenger/views.py
@@ -1,6 +1,7 @@
 from django.template import Context, loader, RequestContext
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render_to_response
+from django.contrib.auth.decorators import login_required
 from main_app.models import Clue
 
 def index(request):
@@ -15,3 +16,9 @@ def view_clue(request, clue_slug):
 		return render_to_response('clue_form.html', {'message': 'Incorrect keyword!'}, context_instance=RequestContext(request))
 	else:
 		return render_to_response('clue_form.html', {'request': request}, context_instance=RequestContext(request))
+		
+@login_required
+def admin_view_clue(request, clue_id):
+	c = get_object_or_404(Clue, id=clue_id)
+	nc = c.next_clue
+	return render_to_response('clue_admin.html', {'clue': c, 'next_clue': nc}, context_instance=RequestContext(request))
