a0cbcebb2c77ba74e093f7b5e4cba83e4ec3403f
Ariana Rizzitano <arizzitano@gmail.com>
2012-08-13 23:13:41 -0400

    reconfig'd for venv setup'

--- django_scavenger/models.py
+++ django_scavenger/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.utils import simplejson
+import os, string, random, urllib, urllib2
+from simplescavenger import settings
 
-# Create your models here.
+class Clue(models.Model):
+	text = models.TextField()
+	code_url = models.URLField(null=True, blank=True)
+	number = models.PositiveIntegerField(default=0)
+	location = models.TextField(null=True, blank=True)
+	hint = models.TextField(null=True, blank=True)
+	keyword = models.CharField(max_length=32, null=True, blank=True)
+	url_slug = models.CharField(max_length=32, null=True, blank=True, unique=True)
+	short_url = models.URLField(max_length=32, null=True, blank=True)
+	next_clue = models.OneToOneField('self', related_name='prev_clue', blank=True, null=True)
+	
+	def __unicode__(self):
+		return 'Clue #%i: %s' % (self.number, self.location)
+
+def finish_clue(sender, instance, created, **kwargs):
+	if created:
+		if not instance.url_slug:
+			instance.url_slug = os.urandom(16).encode('hex')
+		if not instance.keyword:
+			instance.keyword = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(8))
+		if not instance.short_url:
+			long_url = urllib.quote_plus('http://%s/clue/%s' % (settings.DOMAIN, instance.url_slug))
+			r = urllib2.Request('http://is.gd/create.php?format=simple&url=%s' % long_url,)
+			opener = urllib2.build_opener()
+			f = opener.open(r)
+			instance.short_url = f.read()
+		if not instance.code_url:
+			instance.code_url = 'http://api.qrserver.com/v1/create-qr-code/?size=%s&data=%s' % (settings.QR_CODE_SIZE, instance.short_url)
+		instance.save()
+
+post_save.connect(finish_clue, sender=Clue)
--- django_scavenger/views.py
+++ django_scavenger/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.template import Context, loader, RequestContext
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404, render_to_response
+from main_app.models import Clue
+
+def index(request):
+	return render_to_response('index.html', {}, context_instance=RequestContext(request))
+	
+def view_clue(request, clue_slug):
+	c = get_object_or_404(Clue, url_slug=clue_slug)
+	if request.method == 'POST':
+		if request.POST['keyword']:
+			if request.POST['keyword'] == c.keyword:
+				return render_to_response('clue_user.html', {'clue': c}, context_instance=RequestContext(request))
+		return render_to_response('clue_form.html', {'message': 'Incorrect keyword!'}, context_instance=RequestContext(request))
+	else:
+		return render_to_response('clue_form.html', {'request': request}, context_instance=RequestContext(request))
