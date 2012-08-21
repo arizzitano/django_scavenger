a9a284008f2f894e03b85cb08529b71c975d92c4
Ariana Rizzitano <arizzitano@gmail.com>
2012-08-21 18:33:40 -0400

    base email functionality

--- django_scavenger/views.py
+++ django_scavenger/views.py
@@ -1,18 +1,25 @@
 from django.template import Context, loader, RequestContext
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render_to_response
+from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
 from main_app.models import Clue
 from simplescavenger import settings
 
 def index(request):
 	return render_to_response('index.html', {}, context_instance=RequestContext(request))
-	
+
 def view_clue(request, clue_slug):
 	c = get_object_or_404(Clue, url_slug=clue_slug)
 	if request.method == 'POST':
 		if request.POST['keyword']:
 			if request.POST['keyword'] == c.keyword:
+				send_mail('Tomquest Clue Found!',
+				'testy mctesterson',
+				'tomscavengerhunt@gmail.com',
+				['trigger@ifttt.com'],
+				fail_silently=False)
 				return render_to_response('clue_user.html', {'clue': c}, context_instance=RequestContext(request))
 		return render_to_response('clue_form.html', {'message': 'Incorrect keyword!', 'clue':c, 'request':request}, context_instance=RequestContext(request))
 	else:
