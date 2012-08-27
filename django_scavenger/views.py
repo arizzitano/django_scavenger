dd28e087c5e93347d336bdde25ffe6d83a54ffdb
Ariana Rizzitano <arizzitano@gmail.com>
2012-08-27 17:24:34 -0400

    cookies now used for emails

--- django_scavenger/views.py
+++ django_scavenger/views.py
@@ -14,18 +14,13 @@ def view_clue(request, clue_slug):
 	c = get_object_or_404(Clue, url_slug=clue_slug)
 	if request.method == 'POST':
 		if request.POST['keyword']:
-			if request.POST['keyword'].lower() == c.prev_clue.keyword:
-# 				send_mail('Tomquest Clue #%i Found!' % (c.number,),
-# 				render_to_string('checkin_email.html', {'clue': c, 'remaining': (settings.NUM_CLUES - c.number)}),
+			if request.POST['keyword'].lower() == c.prev_clue.keyword.lower():
+# 				send_mail('%s Unlocked Clue #%i!' % (c.number, request.POST['name']),
+# 				render_to_string('checkin_email.html', {'clue': c, 'hunter': request.POST['name'], 'remaining': (settings.NUM_CLUES - c.number)}),
 # 				'tomscavengerhunt@gmail.com',
 # 				settings.EMAIL_LIST,
 # 				fail_silently=False)
 # 				
-# 				send_mail('Tomquest Clue #%i Found!' % (c.number,),
-# 				render_to_string('checkin_email.html', {'clue': c, 'remaining': (settings.NUM_CLUES - c.number - 2)}),
-# 				'tomscavengerhunt@gmail.com',
-# 				['pete.karl@gmail.com'],
-# 				fail_silently=False)
 				return render_to_response('clue_user.html', {'clue': c}, context_instance=RequestContext(request))
 
 		return render_to_response('clue_form.html', {'message': 'Incorrect keyword!', 'clue':c, 'request':request}, context_instance=RequestContext(request))
